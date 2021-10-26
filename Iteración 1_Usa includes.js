8c17edaf0e10dea363f3eeacfcb8be0b4c1c2bb0
Soy Adrián Torres <adriat1994@gmail.com>
2021-10-26 20:42:49 +0200

    Correción de codigo

--- Iteración 1_Usa includes.js
+++ Iteración 1_Usa includes.js
@@ -1,7 +1,7 @@
 const products = ['Camiseta de Pokemon', 'Pantalón coquinero', 'Gorra de gansta', 'Camiseta de Basket', 'Cinrurón de Orión', 'AC/DC Camiseta'];
 
 
-for (i = 0; i < products.length; i++) {
+for (let i = 0; i < products.length; i++) {
     if (products[i].includes("Camiseta")) {
         console.log(true);
     }
